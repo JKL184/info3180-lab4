687a7b958b1117c98a1e81994f7953db12771455
Jordan Lewis <jordankareemlewis@gmail.com>
2021-02-24 00:55:30 -0500

    added file upload

---
+++ app/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import StringField
+from wtforms.validators import DataRequired, Email
+
+class UploadForm(FlaskForm):
+    pic=FileField('Image',validators=[FileRequired(),FileAllowed(['jpg','png','Images only!'])])
